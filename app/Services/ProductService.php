7242d991f653af662452575832fdb320e4a4d1e2
Umid1982 <umidurinov14@gmail.com>
2024-08-19 12:28:22 +0500

    Fix

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\Product;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Http;
 
 class ProductService
 {
@@ -11,16 +12,18 @@ class ProductService
 
     public function getIphoneProducts()
     {
-        $client = new Client();
-        $response = $client->get('https://dummyjson.com/products/search?q=iphone');
+//        $client = new Client();
+//        $response = $client->get('https://dummyjson.com/products/search?q=iphone');
+        $response = Http::get('https://dummyjson.com/products/search?q=iphone');// этот вариант экономит одну строчку!
         $products = json_decode($response->getBody(), true)['products'];
         return $products;
     }
 
     public function storeIphoneProducts(string $query)
     {
-        $client = new Client();
-        $response = $client->get('https://dummyjson.com/products/search?q=' . $query);
+//        $client = new Client();
+//        $response = $client->get('https://dummyjson.com/products/search?q=' . $query);
+        $response = Http::get('https://dummyjson.com/products/search?q=' . $query);// этот вариант экономит одну строчку!
         $products = json_decode($response->getBody(), true)['products'];
 
         $savedProducts = [];
